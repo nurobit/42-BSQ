dc21f4d4148a37050fd853d1b72706114a2517c1
Victor Wade <vwade@e1z1r2p3.42.us.org>
2017-07-18 15:04:40 -0700

    Uploaded [2017:07:18-150440] version[N]

--- sources/ft_bsq.c
+++ sources/ft_bsq.c
@@ -6,25 +6,44 @@
 /*   By: vwade <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/18 14:03:46 by vwade             #+#    #+#             */
-/*   Updated: 2017/07/18 14:12:06 by vwade            ###   ########.fr       */
+/*   Updated: 2017/07/18 15:04:34 by vwade            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_func.h"
+#include <fcntl.h>
+#include <unistd.h>
+#define BUF_SIZE 42
 
-int		main(int ac, char **av)
+typedef	struct	s_coord
 {
-	int i;
+	int			x;
+	int			y;
+}				t_coord;
 
-	i = 0;
-	if (ac == 2)
-	{
-		ft_bsq(av[1]);
-		ft_putchar('\n');
-		return(0);
-	}
-	else if (ac > 2)
-		while (++i < ac)
-			ft_bsq(av[i]);
-	return(0);
+/*
+**	BSQ
+**	Read file to variable
+*/
+
+int		ft_nlines(char *str)
+{
+	int	n;	
+	n = 0;
+	while (*str != '\0')
+		if (*str++ == '\n')
+			n++;
+	return (n);
+}
+
+void	ft_bsq(char *m)
+{
+	int 	fd;
+	int		nlines;
+	char	*buf[BUF_SIZE + 1];
+
+	nlines = 0;
+	fd = open(m, O_RDONLY);
+	while (read(fd, buf, BUF_SIZE)
+			nlines = nlines + ft_nlines(buf);
+	
 }
