a8b14bd0512ab0ac7b1c9a6d3c349bd9e081fa24
Victor Wade <vwade@e1z1r2p3.42.us.org>
2017-07-18 14:46:30 -0700

    Uploaded [2017:07:18-144630] version[N]

---
+++ sources/ft_func.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_func.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vwade <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/18 14:35:32 by vwade             #+#    #+#             */
+/*   Updated: 2017/07/18 14:46:10 by vwade            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_FUNC_H
+# define FT_FUNC_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include "ft_putchar.c"
+# include "ft_putstr.c"
+# include "ft_strlen.c"
+# include "ft_strcmp.c"
+#endif
